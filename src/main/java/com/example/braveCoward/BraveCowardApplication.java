7d5ea3f11b1f954411c899991b1c81e49fb6d097
dradnats1012 <herjebi1012@gmail.com>
2024-10-10 23:38:38 +0900

    init commit

---
+++ src/main/java/com/example/braveCoward/BraveCowardApplication.java
@@ -0,0 +1,12 @@
+package com.example.braveCoward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveCowardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraveCowardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/braveCoward/BraveCowardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.braveCoward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraveCowardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
