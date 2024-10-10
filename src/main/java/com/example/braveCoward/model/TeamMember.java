9a2d437688084d6c75fe41c58cd96d0b20c491b5
dradnats1012 <herjebi1012@gmail.com>
2024-10-11 02:09:13 +0900

    feat: 초기 DB 코드

---
+++ src/main/java/com/example/braveCoward/model/TeamMember.java
@@ -0,0 +1,41 @@
+package com.example.braveCoward.model;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "team_member")
+@NoArgsConstructor(access = PROTECTED)
+public class TeamMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "position", nullable = false)
+    private String position;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
