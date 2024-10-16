b337362dd366b1dd747c48c40564665c66fabe1d
dradnats1012 <herjebi1012@gmail.com>
2024-10-16 20:10:25 +0900

    feat: CORS를 위한 WebMVCConfig 클래스 생성

---
+++ src/main/java/com/example/braveCoward/config/WebMVCConfig.java
@@ -0,0 +1,20 @@
+package com.example.braveCoward.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowCredentials(true);
+    }
+}
