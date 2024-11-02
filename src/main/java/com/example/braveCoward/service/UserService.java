34162b0a4dc80b13818aa44b1df4bed4cba6d65a
seongjae6751 <seongjae6751@naver.com>
2024-11-02 23:41:00 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/example/braveCoward/service/UserService.java
+++ src/main/java/com/example/braveCoward/service/UserService.java
@@ -3,14 +3,18 @@ package com.example.braveCoward.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.braveCoward.dto.MembersResponse;
+import com.example.braveCoward.dto.UserRegisterRequest;
 import com.example.braveCoward.model.Project;
 import com.example.braveCoward.model.TeamMember;
+import com.example.braveCoward.model.User;
 import com.example.braveCoward.repository.ProjectRepository;
 import com.example.braveCoward.repository.TeamMemberRepository;
+import com.example.braveCoward.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,11 +24,26 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public MembersResponse getTeamMembers(Long projectId) {
         Project project = projectRepository.findById(projectId).get();
         List<TeamMember> teamMembers = project.getTeam().getTeamMembers();
 
         return MembersResponse.from(teamMembers);
+    }
+
+    public void userRegister(UserRegisterRequest request) {
+        if (userRepository.findByEmail(request.email()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        User user = User.builder()
+            .password(passwordEncoder.encode(request.password()))
+            .name(request.name())
+            .email(request.email())
+            .build();
+        userRepository.save(user);
     }
 }
