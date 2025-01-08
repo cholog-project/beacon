b3dab9647b08416180a93526f8232614447b58ce
joamksh <joamksh@naver.com>
2025-01-08 16:50:22 +0900

    feat : project 단일 조회 api 구현

--- src/main/java/com/example/braveCoward/controller/ProjectController.java
+++ src/main/java/com/example/braveCoward/controller/ProjectController.java
@@ -2,12 +2,13 @@ package com.example.braveCoward.controller;
 
 import com.example.braveCoward.dto.project.ProjectCreateRequest;
 import com.example.braveCoward.dto.project.ProjectCreateResponse;
+import com.example.braveCoward.dto.project.ProjectResponse;
 import com.example.braveCoward.service.ProjectService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/projects/{teamId}")
+@RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
@@ -16,11 +17,17 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    @PostMapping
+    @PostMapping("/{teamId}")
     public ResponseEntity<ProjectCreateResponse> createProject(
             @PathVariable Long teamId,
             @RequestBody ProjectCreateRequest request) {
         ProjectCreateResponse response = projectService.createProject(teamId, request);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<ProjectResponse> getProject(@PathVariable Long projectId) {
+        ProjectResponse response = projectService.getProject(projectId);
         return ResponseEntity.status(201).body(response);
     }
 }
