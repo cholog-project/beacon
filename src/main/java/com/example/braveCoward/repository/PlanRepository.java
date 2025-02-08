79621e05040dffb54a147e52b54c01545e011faa
dradnats1012 <herjebi1012@gmail.com>
2025-02-08 15:18:43 +0900

    feat: 검색 관련 repository 메서드 추가

--- src/main/java/com/example/braveCoward/repository/PlanRepository.java
+++ src/main/java/com/example/braveCoward/repository/PlanRepository.java
@@ -1,12 +1,16 @@
 package com.example.braveCoward.repository;
 
 import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
+
 import com.example.braveCoward.model.Plan;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -25,4 +29,10 @@ public interface PlanRepository extends Repository<Plan, Long> {
     Page<Plan> findAllByProjectId(Long projectId, Pageable pageable);
 
     List<Plan> findByEndDate(LocalDate endDate);
+
+    Page<Plan> findAllByTitleContainsOrDescriptionContains(String keyword, String keyword2, Pageable pageable);
+
+    Page<Plan> findAllByTitleContains(String keyword, Pageable pageable);
+
+    Page<Plan> findAllByDescriptionContains(String keyword, Pageable pageable);
 }
