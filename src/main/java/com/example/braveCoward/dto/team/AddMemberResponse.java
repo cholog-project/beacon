1b9663f7b487b444b1e6d5e0ca62059da130eb4f
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-02-12 18:18:46 +0900

    Refactor: dto 개행 추가

--- src/main/java/com/example/braveCoward/dto/team/AddMemberResponse.java
+++ src/main/java/com/example/braveCoward/dto/team/AddMemberResponse.java
@@ -1,3 +1,10 @@
 package com.example.braveCoward.dto.team;
 
-public record AddMemberResponse(Long memberId, String role, String position, Long teamId, Long userId) {}
+public record AddMemberResponse(
+        Long memberId,
+        String role,
+        String position,
+        Long teamId,
+        Long userId
+) {
+}
