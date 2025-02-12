a03aa805543882cadef4ec837a1410f9ec70450b
joamksh <joamksh@naver.com>
2025-02-12 20:28:24 +0900

    ref : swagger 알람테스트 바로 실행

--- src/main/java/com/example/braveCoward/controller/AlarmController.java
+++ src/main/java/com/example/braveCoward/controller/AlarmController.java
@@ -3,6 +3,7 @@ package com.example.braveCoward.controller;
 import com.example.braveCoward.model.User;
 import com.example.braveCoward.repository.UserRepository;
 import com.example.braveCoward.service.AlarmService;
+import com.example.braveCoward.service.PlanNotificationScheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AlarmController {
     private final AlarmService alarmService;
-    private final UserRepository userRepository;
+    private final PlanNotificationScheduler planNotificationScheduler;
 
-    @GetMapping("/send-email")
-    public String sendEmailToUser(@RequestParam Long userId, @RequestParam String description) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 사용자가 없습니다."));
-
-        return alarmService.sendEmailToUser(user, description);
+    @GetMapping("/send-test-email")
+    public String sendScheduledEmailNow() {
+        planNotificationScheduler.sendPlanNotifications();
+        return "스케줄링된 이메일이 즉시 실행되었습니다.";
     }
 }
