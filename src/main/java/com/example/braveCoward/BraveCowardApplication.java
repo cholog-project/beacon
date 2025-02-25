0e3930e411dc8225b2edb0ca30365bc0c1045a5d
dradnats1012 <herjebi1012@gmail.com>
2025-02-25 20:56:58 +0900

    feat: Batch 허용 어노테이션 추가

--- src/main/java/com/example/braveCoward/BraveCowardApplication.java
+++ src/main/java/com/example/braveCoward/BraveCowardApplication.java
@@ -1,5 +1,6 @@
 package com.example.braveCoward;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableBatchProcessing
 public class BraveCowardApplication {
 
     public static void main(String[] args) {
